b7c060abd18b6daf68bb07a322acdee259a3ca38
Wladislaw Martaler <wladislaw.martaler@outlook.com>
2021-10-17 21:00:46 +0200

    Add MongoDB Upsert

--- src/databases/mongodb_services/mongodb_service.py
+++ src/databases/mongodb_services/mongodb_service.py
@@ -8,28 +8,23 @@ def init_database():
     try:
         global conn
         conn = MongoClient(
-            host=os.environ['MONGODB_DOMAIN'],
-            port=int(os.environ['MONGODB_PORT']),
-            username=os.environ['MONGO_ROOT_USER'],
-            password=os.environ['MONGO_ROOT_PASSWORD'],
+            host=os.environ.get('MONGODB_DOMAIN'),
+            port=os.environ.get('MONGODB_PORT'),
+            username=os.environ.get('MONGO_ROOT_USER'),
+            password=os.environ.get('MONGO_ROOT_PASSWORD'),
         )
         print("Debug: Database connection established")
+        print("MondoDB-Server version:", conn.server_info()["version"])
+        # database_names = conn.list_database_names()
+        # print("\ndatabases:", database_names)
+
         return True
     except errors.PyMongoError as e:
         print(f"Error connecting to MongoDB Platform: {e}")
         sys.exit(1)
 
-def get_something_from_database():
-    print("MondoDB-Server version:", conn.server_info()["version"])
-    database_names = conn.list_database_names()
-    print("\ndatabases:", database_names)
-    mydb = conn["Datenbank"]
-    mycol = mydb["Tabelle"]
-    abstract = { "nodes": [1, 2, 3] , "edges": ['a', 'b', 'c']}
-    epk = { "nodes": [1, 2, 3] , "edges": ['a', 'b', 'c']}
-    bpmn = { "nodes": [1, 2, 3] , "edges": ['a', 'b', 'c']}
-    mydict = {"abstract": abstract, "epk": epk, "bpmn": bpmn}
-    x = mycol.insert_one(mydict)
-    # mydb[mycol].createIndex({"SAP-ID": 1}, unique=True)
-    # for i in mycol.find():
-    #     print(i)
+def upsert(id, graph_dictionary):
+    graph_database = conn["graph"]
+    mongo_collection = graph_database["graph"]
+    mongo_collection.replace_one({"_id": id}, graph_dictionary, upsert=True)
+    print(f"Upsert {id} to Database")
