258037831c96dea1559ba942de933932cde4eda9
IljaAndrejewitsch <ilyabudnikov23@gmail.com>
2021-10-17 15:18:21 +0200

    More Fixes

--- src/eventlog/eventlog.py
+++ src/eventlog/eventlog.py
@@ -1,9 +1,12 @@
+from src.model.variant import Variant
+
+
 class EventLog:
     sequences = []
 
     def variants(self):
         variants_by_footprint = {}
         for s in self.sequences:
-            variants_by_footprint.setdefault(s.footprint(), []).append(s)
+            variants_by_footprint.setdefault(s.footprint(), Variant(s)).add_sequence(s)
 
         return variants_by_footprint.values()
